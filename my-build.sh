de8ea0dca6aa07034a47c8cc01b216e0640452fd
Robert Ying <yingrui205@gmail.com>
2019-03-10 04:00:08 +0800

    Add my-build.sh

---
+++ my-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export ANDROID_NDK_HOME=$HOME/Library/Android/sdk/ndk-bundle
+export HOST_TAG=darwin-x86_64
+export MIN_SDK_VERSION=21
+
+export CFLAGS="-Os -ffunction-sections -fdata-sections -fno-unwind-tables -fno-asynchronous-unwind-tables"
+export LDFLAGS="-Wl,-s -Wl,-Bsymbolic -Wl,--gc-sections"
+
+./build.sh
