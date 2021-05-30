1e8c3553ad54e92f5d0b579f9c84405df8b8cb28
Rui Ying <yingrui205@gmail.com>
2021-05-30 13:41:50 +0800

    Remove shared library flags

--- my-build.sh
+++ my-build.sh
@@ -4,7 +4,7 @@ export NDK=$HOME/Library/Android/sdk/ndk/22.1.7171670
 export HOST_TAG=darwin-x86_64
 export MIN_SDK_VERSION=21
 
-export CFLAGS="-Os -ffunction-sections -fdata-sections -fno-unwind-tables -fno-asynchronous-unwind-tables"
-export LDFLAGS="-Wl,-s -Wl,-Bsymbolic -Wl,--gc-sections"
+export CFLAGS="-Os"
+export LDFLAGS="-Wl,-Bsymbolic"
 
 ./build.sh
